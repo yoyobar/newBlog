20948b5463267b024205fc0f8b76e7283e539316
barwait <barwait@naver.com>
2024-05-31 19:56:31 +0900

    fix: server component 환경에서 localstorage 사용불가, 롤백

--- store.tsx
+++ store.tsx
@@ -1,28 +1,14 @@
-'use client';
-
 import { create } from 'zustand';
 
 interface AnimationStore {
     running: boolean;
     onToggle: () => void;
 }
-const loadSetting = JSON.parse(localStorage.getItem('animation') as string);
 
-const useAnimation = create<AnimationStore>((set) => {
-    const running = loadSetting ? loadSetting.snow : true;
+const useAnimation = create<AnimationStore>((set) => ({
+    running: true,
 
-    set({ running: running });
-
-    return {
-        running: running,
-
-        onToggle: () =>
-            set((state) => {
-                const newState = !state.running;
-                localStorage.setItem('animation', JSON.stringify({ snow: newState }));
-                return { running: newState };
-            }),
-    };
-});
+    onToggle: () => set((state) => ({ running: !state.running })),
+}));
 
 export default useAnimation;
