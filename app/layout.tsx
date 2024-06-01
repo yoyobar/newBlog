3d459aa9e3eee44c0fa3ef31862a2a82234a7707
barwait <barwait@naver.com>
2024-06-01 18:58:01 +0900

    close: #3 feat: Favicon 등록 및 구별, webmanifest 작성, HTML내 등록

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,23 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-    title: 'New Blog',
+    title: 'Trouble Wiki',
     description: 'Next.js로 구현하는 인터랙티브 블로그 프로젝트',
+    icons: {
+        icon: [
+            { rel: 'apple-touch-icon', sizes: '180x180', url: '/favicon/apple-touch-icon.png' },
+            { rel: 'icon', type: 'image/png', sizes: '32x32', url: '/favicon/favicon-32x32.png' },
+            { rel: 'icon', type: 'image/png', sizes: '16x16', url: '/favicon/favicon-16x16.png' },
+        ],
+    },
+    manifest: '/favicon/site.webmanifest',
+};
+
+export const viewport = {
+    themeColor: '#000000',
+    msapplication: {
+        TileColor: '#2b5797',
+    },
 };
 
 export default function RootLayout({
--- components/Dock.tsx
+++ components/Dock.tsx
@@ -1,5 +1,6 @@
+'use client';
+
 import Image from 'next/image';
-import React from 'react';
 const Dock = () => {
     const dockData = [
         {
