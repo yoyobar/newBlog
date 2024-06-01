81c6a978c4011ef0b8f3394d1c000b76d13e2db7
barwait <barwait@naver.com>
2024-06-01 20:34:29 +0900

    fix: favicon.ico 에러 수정

--- components/Dock.tsx
+++ components/Dock.tsx
@@ -2,35 +2,34 @@
 
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-
-enum DataType {
-    WIKI = 'Wiki',
-    CALENDAR = 'Calendar',
-    COMMENTS = 'Comments',
-    SETTING = 'Setting',
-}
-
-const dockData = [
-    {
-        src: 'https://upload.wikimedia.org/wikipedia/commons/e/e9/Notion-logo.svg',
-        text: 'Wiki',
-    },
-    {
-        src: 'https://upload.wikimedia.org/wikipedia/commons/a/a5/Google_Calendar_icon_%282020%29.svg',
-        text: 'Calendar',
-    },
-    {
-        src: 'https://upload.wikimedia.org/wikipedia/commons/5/51/IMessage_logo.svg',
-        text: 'Comments',
-    },
-    {
-        src: 'https://upload.wikimedia.org/wikipedia/commons/a/ac/Windows_Settings_icon.svg',
-        text: 'Setting',
-    },
-];
 
 const Dock = () => {
+    enum DataType {
+        WIKI = 'Wiki',
+        CALENDAR = 'Calendar',
+        COMMENTS = 'Comments',
+        SETTING = 'Setting',
+    }
+
+    const dockData = [
+        {
+            src: 'https://upload.wikimedia.org/wikipedia/commons/e/e9/Notion-logo.svg',
+            text: 'Wiki',
+        },
+        {
+            src: 'https://upload.wikimedia.org/wikipedia/commons/a/a5/Google_Calendar_icon_%282020%29.svg',
+            text: 'Calendar',
+        },
+        {
+            src: 'https://upload.wikimedia.org/wikipedia/commons/5/51/IMessage_logo.svg',
+            text: 'Comments',
+        },
+        {
+            src: 'https://upload.wikimedia.org/wikipedia/commons/a/ac/Windows_Settings_icon.svg',
+            text: 'Setting',
+        },
+    ];
+
     const router = useRouter();
 
     const selectHandler = (select: string) => {
