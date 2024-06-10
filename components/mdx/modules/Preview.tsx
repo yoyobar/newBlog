c20b3e96c63ff1a13189b3c06bbcf22db139c79d
barwait <barwait@naver.com>
2024-06-10 10:38:33 +0900

    doc: module test page 옮겨서 작성, 스타일 통일화 #6

--- components/mdx/modules/Preview.tsx
+++ components/mdx/modules/Preview.tsx
@@ -1,10 +1,16 @@
 'use client';
 
-export const Preview = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <>
-      <div className="text-sky-700 text-bold">Preview</div>
-      <div className="p-2 w-full rounded-md text-black">{children}</div>
-    </>
-  );
-};
+export function Preview({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <>
+            <div className='text-sky-700 dark:text-slate-200 font-bold pl-2 pb-2'>Preview</div>
+            <div className={`p-2 border w-full rounded-md text-black dark:text-white dark:border-[#1e191e]  dark:bg-[#362e36]`}>
+                {children}
+            </div>
+        </>
+    );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,149 +1,149 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  darkMode: 'class',
-  content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
-  theme: {
-    extend: {
-      transitionProperty: {
-        'width-height': 'width, height',
-      },
-      keyframes: {
-        popUp: {
-          '0%': {
-            top: '0',
-            opacity: '0.1',
-          },
-          '100%': {
-            top: '3rem',
-            opacity: '0.9',
-          },
-        },
-
-        pageOn: {
-          '0%': {
-            scale: '0%',
-          },
-          '100%': {
-            scale: '100%',
-          },
-        },
-      },
-      animation: {
-        popUp: 'popUp 0.3s ease-out 1',
-        pageOn: 'pageOn 0.3s ease-out 1',
-      },
-      colors: {
-        background: {
-          DEFAULT: '#222222',
-        },
-        header: {
-          DEFAULT: '#302b25',
-          text: '#ddd',
-        },
-      },
-      typography: {
-        DEFAULT: {
-          css: {
-            // ! <p> 태그
-            p: {
-              marginTop: '2rem',
-              marginBottom: '2rem',
+    darkMode: 'class',
+    content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
+    theme: {
+        extend: {
+            transitionProperty: {
+                'width-height': 'width, height',
             },
-
-            // ! 인라인 코드 블럭
-            ':not(pre) > code': {
-              display: 'inline-block',
-              color: 'red',
-              fontWeight: 'inherit',
-              margin: '0.2rem 1rem',
-              backgroundColor: '#bae6fd',
-              fontFamily: '"SFMono-Regular", Menlo, Consolas, "PT Mono", "Liberation Mono", Courier, monospace',
-              borderRadius: 3,
-              padding: '0.2rem 0.4rem',
-              overflowWrap: 'break-word',
-              textAlign: 'center',
-            },
-
-            '.dark :not(pre) > code': {
-              backgroundColor: '#282a37',
-              color: 'orange',
-            },
-
-            'code::before': {
-              content: 'none',
-            },
-
-            'code::after': {
-              content: 'none',
-            },
-
-            // ! 넘버링 문법
-            code: {
-              counterReset: 'line',
-            },
-
-            // ! 넘버링 스타일
-            'code[data-line-numbers] > [data-line]::before': {
-              counterIncrement: 'line',
-              content: 'counter(line)',
-
-              display: 'inline-block',
-              width: '1rem',
-              marginRight: '1.4rem',
-              textAlign: 'right',
-              color: 'lightgrey',
-              fontSize: '1.2rem',
-            },
-            'code[data-line-numbers-max-digits="2"] > [data-line]::before': {
-              width: '1rem',
-            },
-            'code[data-line-numbers-max-digits="3"] > [data-line]::before': {
-              width: '2rem',
-            },
-
-            //! 코드 블럭
-            pre: {
-              backgroundColor: 'var(--shiki-light-bg)',
-              border: '1px solid #e5e7eb',
-
-              '@media (max-width: 1024px)': {
-                pre: {
-                  width: '100%',
+            keyframes: {
+                popUp: {
+                    '0%': {
+                        top: '0',
+                        opacity: '0.1',
+                    },
+                    '100%': {
+                        top: '3rem',
+                        opacity: '0.9',
+                    },
                 },
-              },
-            },
 
-            '.dark pre': {
-              backgroundColor: 'var(--shiki-dark-bg)',
-              color: 'var(--shiki-dark)',
-              border: '1px solid #374151',
+                pageOn: {
+                    '0%': {
+                        scale: '0%',
+                    },
+                    '100%': {
+                        scale: '100%',
+                    },
+                },
             },
-            'pre > code > span': {
-              padding: '1.1rem',
-              paddingRight: '1.1rem',
+            animation: {
+                popUp: 'popUp 0.3s ease-out 1',
+                pageOn: 'pageOn 0.3s ease-out 1',
             },
-            'pre code span': {
-              color: 'var(--shiki-light)',
+            colors: {
+                background: {
+                    DEFAULT: '#222222',
+                },
+                header: {
+                    DEFAULT: '#302b25',
+                    text: '#ddd',
+                },
             },
-            '.dark pre code span': {
-              color: 'var(--shiki-dark)',
-            },
-            '[data-highlighted-line]': {
-              backgroundColor: 'rgba(253, 224, 71, 0.2)',
-            },
+            typography: {
+                DEFAULT: {
+                    css: {
+                        // ! <p> 태그
+                        p: {
+                            marginTop: '2rem',
+                            marginBottom: '2rem',
+                        },
 
-            //! 언더라인
-            u: {
-              textUnderlineOffset: '4px',
-              textDecorationThickness: 1,
-              fontWeight: 600,
+                        // ! 인라인 코드 블럭
+                        ':not(pre) > code': {
+                            display: 'inline-block',
+                            color: 'red',
+                            fontWeight: 'inherit',
+                            margin: '0.2rem 1rem',
+                            backgroundColor: '#bae6fd',
+                            fontFamily: '"SFMono-Regular", Menlo, Consolas, "PT Mono", "Liberation Mono", Courier, monospace',
+                            borderRadius: 3,
+                            padding: '0.2rem 0.4rem',
+                            overflowWrap: 'break-word',
+                            textAlign: 'center',
+                        },
+
+                        '.dark :not(pre) > code': {
+                            backgroundColor: '#282a37',
+                            color: 'orange',
+                        },
+
+                        'code::before': {
+                            content: 'none',
+                        },
+
+                        'code::after': {
+                            content: 'none',
+                        },
+
+                        // ! 넘버링 문법
+                        code: {
+                            counterReset: 'line',
+                        },
+
+                        // ! 넘버링 스타일
+                        'code[data-line-numbers] > [data-line]::before': {
+                            counterIncrement: 'line',
+                            content: 'counter(line)',
+
+                            display: 'inline-block',
+                            width: '1rem',
+                            marginRight: '1.4rem',
+                            textAlign: 'right',
+                            color: 'lightgrey',
+                            fontSize: '1.2rem',
+                        },
+                        'code[data-line-numbers-max-digits="2"] > [data-line]::before': {
+                            width: '1rem',
+                        },
+                        'code[data-line-numbers-max-digits="3"] > [data-line]::before': {
+                            width: '2rem',
+                        },
+
+                        //! 코드 블럭
+                        pre: {
+                            backgroundColor: 'var(--shiki-light-bg)',
+                            border: '1px solid #e5e7eb',
+
+                            '@media (max-width: 1024px)': {
+                                pre: {
+                                    width: '100%',
+                                },
+                            },
+                        },
+
+                        '.dark pre': {
+                            backgroundColor: 'var(--shiki-dark-bg)',
+                            color: 'var(--shiki-dark)',
+                            border: '1px solid #374151',
+                        },
+                        'pre > code > span': {
+                            padding: '1.1rem',
+                            paddingRight: '1.1rem',
+                        },
+                        'pre code span': {
+                            color: 'var(--shiki-light)',
+                        },
+                        '.dark pre code span': {
+                            color: 'var(--shiki-dark)',
+                        },
+                        '[data-highlighted-line]': {
+                            backgroundColor: 'rgba(253, 224, 71, 0.2)',
+                        },
+
+                        //! 언더라인
+                        u: {
+                            textUnderlineOffset: '4px',
+                            textDecorationThickness: 1,
+                            fontWeight: 600,
+                        },
+                    },
+                },
             },
-          },
         },
-      },
     },
-  },
-  plugins: [require('@tailwindcss/typography')],
+    plugins: [require('@tailwindcss/typography')],
 } satisfies Config;
 export default config;
