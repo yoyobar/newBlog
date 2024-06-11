dcbf11e6c0b55f149803c85e5433802a1f536c23
barwait <barwait@naver.com>
2024-06-11 20:02:03 +0900

    style: 카테고리 선택바 색상추가 #6

--- components/posts/SubTitle.tsx
+++ components/posts/SubTitle.tsx
@@ -10,12 +10,12 @@ const SubTitle = ({ type }: { type: string }) => {
 
     return (
         <>
-            <div className='flex items-center gap-8 px-4 py-4 transition hover:bg-slate-300 dark:hover:bg-slate-950 text-black dark:text-white rounded-md'>
-                <div className={`${boxClassName} text-3xl p-3 rounded-full dark:bg-slate-700 bg-slate-200`}>
+            <div className='flex items-center gap-8 px-4 py-4 hover:bg-slate-300 text-3xl hover:text-sky-500 transition-all dark:hover:bg-slate-950 text-black dark:text-white rounded-md'>
+                <div className={`${boxClassName} p-3 rounded-full dark:bg-slate-700 bg-slate-200`}>
                     <Icon />
                 </div>
-                <div className='text-3xl flex-grow'>{titleName}</div>
-                <div className='text-sky-500 scale-y-[200%]'>{'>'}</div>
+                <div className='flex-grow text-3xl'>{titleName}</div>
+                <div className='text-sky-500 scale-y-[200%] text-xl'>{'>'}</div>
             </div>
         </>
     );
