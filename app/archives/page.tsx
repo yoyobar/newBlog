da3fff2320da6f345357d5e804b050f681d757aa
barwait <barwait@naver.com>
2024-06-12 20:05:04 +0900

    Suspense 추가로 params 데이터 로딩 부여

--- app/archives/page.tsx
+++ app/archives/page.tsx
@@ -2,11 +2,13 @@ import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
 import PageContainer from '@/components/PageContainer';
-import { allBrowseLoad, allFilesLoad } from '@/utils/parseData';
 import dayjs from 'dayjs';
 import readingTime from 'reading-time';
 import Title from '@/components/posts/Title';
 import Archives from '@/components/posts/Archives';
+import { allBrowseLoad, allFilesLoad } from '@/utils/parseData';
+import { AllBrowse } from '../posts/page';
+import { Suspense } from 'react';
 
 const BASE_DIR = 'posts';
 
@@ -17,43 +19,41 @@ export type PostMatter = {
     date: string;
 };
 
-export default async function Home({ searchParams }: { searchParams?: { [key: string]: string | string[] | undefined } }) {
-    async function fetchData() {
-        const { category, mdx } = allBrowseLoad();
-        const categoryFiles = allFilesLoad(category);
-        const allFiles = [...mdx.map((file) => ({ category: '', file })), ...categoryFiles];
-        const tag = searchParams?.tag === undefined ? '' : String(searchParams.tag);
+// 홈 컴포넌트
+export default function ArchivesPage() {
+    const { category, mdx }: AllBrowse = allBrowseLoad();
+    const categoryFiles = allFilesLoad(category);
+    const allFiles = [...mdx.map((file) => ({ category: '', file })), ...categoryFiles];
 
-        const blogs = await Promise.all(
-            allFiles.map(async ({ category, file }) => {
-                const filePath = category ? path.join(BASE_DIR, category, file) : path.join(BASE_DIR, file);
-                const fileContent = fs.readFileSync(filePath, 'utf-8');
-                const { content, data } = matter(fileContent);
-                const grayMatter = data as PostMatter;
+    const blogs = allFiles.map(({ category, file }) => {
+        const filePath = category ? path.join(BASE_DIR, category, file) : path.join(BASE_DIR, file);
+        const fileContent = fs.readFileSync(filePath, 'utf-8');
+        const { content, data } = matter(fileContent);
+        const grayMatter = data as PostMatter;
 
-                return {
-                    meta: {
-                        title: grayMatter.title,
-                        length: allFiles.length,
-                        tags: grayMatter.tags,
-                        date: dayjs(grayMatter.date).format('YYYY-MM-DD'),
-                        readingMinutes: Math.ceil(readingTime(content).minutes),
-                    },
-                    slug: category ? `${category}/${file.replace('.mdx', '')}` : file.replace('.mdx', ''),
-                };
-            })
-        );
+        return {
+            meta: {
+                title: grayMatter.title,
+                length: allFiles.length,
+                tags: grayMatter.tags,
+                date: dayjs(grayMatter.date).format('YYYY-MM-DD'),
+                readingMinutes: Math.ceil(readingTime(content).minutes),
+            },
+            slug: category ? `${category}/${file.replace('.mdx', '')}` : file.replace('.mdx', ''),
+        };
+    });
 
-        return { blogs, tag };
-    }
-
-    const { blogs, tag } = await fetchData();
+    const tags = blogs.flatMap((blog) => blog.meta.tags);
+    const filteredTags = Array.from(new Set(tags));
+    const sortedTags = filteredTags.sort((a, b) => a.localeCompare(b));
 
     return (
         <PageContainer>
             <div className='w-full xl:w-[90%] m-auto select-none'>
                 <Title type={'tags'} />
-                <Archives tag={tag} blogs={blogs} />
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Archives blogs={blogs} tag={sortedTags} />
+                </Suspense>
             </div>
         </PageContainer>
     );
--- components/posts/Archives.tsx
+++ components/posts/Archives.tsx
@@ -4,6 +4,7 @@ import TagList from './TagList';
 import Browse from './Browse';
 import { MdSort } from 'react-icons/md';
 import { MdClose } from 'react-icons/md';
+import { useSearchParams } from 'next/navigation';
 
 type AllPostsProp = {
     meta: {
@@ -15,28 +16,31 @@ type AllPostsProp = {
     slug: string;
 }[];
 
-const Archives = ({ blogs, tag }: { blogs: AllPostsProp; tag: string }) => {
+const Archives = ({ tag, blogs }: { tag: string[]; blogs: AllPostsProp }) => {
     const [selected, setSelected] = useState('');
+    const params = useSearchParams();
+    const tagData = params.get('tag');
 
     useEffect(() => {
-        setSelected(tag);
-    }, [tag]);
+        if (tagData) {
+            setSelected(tagData);
+        } else {
+            setSelected('');
+        }
+    }, [tagData]);
 
     const selectHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
         setSelected(e.currentTarget.value);
     };
 
-    const tags = blogs.flatMap((blog) => blog.meta.tags);
-    const filteredTags = Array.from(new Set(tags));
-    const sortedTags = filteredTags.sort((a, b) => a.localeCompare(b));
     return (
         <>
             <div className='flex flex-col gap-8'>
                 <div className='text-sky-200xl'>태그에 해당하는 게시글을 조회합니다.</div>
-                <TagList onSelect={selectHandler} tags={sortedTags} />
+                <TagList onSelect={selectHandler} tags={tag} />
                 <div className='flex gap-2 text-4xl items-center'>
                     <MdSort className='text-sky-600' />
-                    {selected}
+                    {selected === '' ? 'ALL' : selected}
                     {selected !== '' && <MdClose onClick={() => setSelected('')} className='cursor-pointer text-3xl mt-2 text-rose-600' />}
                 </div>
                 <Browse blogs={blogs} selected={selected} />
