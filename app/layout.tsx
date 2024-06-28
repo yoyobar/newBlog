bf385862fc6b6e055488fe0d71ed6ccfebce015f
이름 <barwait@naver.com>
2024-06-28 18:03:05 +0900

    fix: App-router

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,8 @@ import Provider from './Provider';
 import { Metadata, MetadataRoute } from 'next';
 import localFont from 'next/font/local';
 import { siteConfig } from '@/config/siteconfig';
-import Script from 'next/script';
+import { Suspense } from 'react';
+import Analytic from './Analytics';
 
 const pretendard = localFont({
     src: '../public/fonts/PretendardVariable.woff2',
@@ -48,38 +49,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <body
                 className={`${pretendard.className} fixed bg-cover bg-center overflow-hidden w-full h-full bg-dark bg-background`}
             >
+                <Suspense>
+                    <Analytic />
+                </Suspense>
                 <Provider>
-                    <Analytics />
                     <Header />
                     {children}
                     <Dock />
                 </Provider>
             </body>
         </html>
-    );
-}
-
-export function Analytics() {
-    return (
-        <>
-            {/* <!-- Google Tag Manager (noscript) --> */}
-            <noscript>
-                <iframe
-                    src={`https://www.googletagmanager.com/ns.html?id=G-5ET7D32N8M`}
-                    height='0'
-                    width='0'
-                    className='hidden invisible'
-                ></iframe>
-            </noscript>
-            {/* <!-- End Google Tag Manager (noscript) --> */}
-            {/* <!-- Google Tag Manager --> */}
-            <Script id='google-tag-management'>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer',"G-5ET7D32N8M");
-  
-  `}</Script>
-        </>
     );
 }
