adf3f90376fed66abe6d1b3a301bd1364c9c543d
이름 <barwait@naver.com>
2024-07-05 22:15:36 +0900

    rollback

--- components/mdx/footer/index.tsx
+++ components/mdx/footer/index.tsx
@@ -1,26 +1,20 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { FootMatterTypes } from '@/config/types';
 import { usePathname, useRouter } from 'next/navigation';
 import { useCopyToClipboard } from 'react-use';
 import CopyRight from './CopyRight';
 
 import Card from './Card';
-import LikeButton from './LikeButton';
-import CommentComponent from './Comment/Comment';
+import Comment from './Comment/Comment';
+
 const Mdx_Footer = ({ footMatter }: { footMatter: FootMatterTypes[] }) => {
     const [copiedText, copy] = useCopyToClipboard();
     const [copyStatus, setCopyStatus] = useState<boolean>(false);
-    const [postId, setPostId] = useState<string | null>(null);
     const path = usePathname();
     const folderPath = path.split('/')[2];
     const nowPath = path.split('/')[3];
     const router = useRouter();
-
-    useEffect(() => {
-        // 여기서 postId를 설정합니다. 실제 구현에서는 이 값을 서버에서 가져오거나 다른 방식으로 설정할 수 있습니다.
-        setPostId(nowPath);
-    }, [nowPath]);
 
     //? footMatter 배열을 날짜 기준으로 오름차순 정렬
     const sortedFootMatter = [...footMatter].sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
@@ -40,9 +34,6 @@ const Mdx_Footer = ({ footMatter }: { footMatter: FootMatterTypes[] }) => {
         <>
             <div className="w-full border-b border-black dark:border-gray-400 mt-10 mb-10"></div>
             <div>
-                <div className="flex justify-center mb-8">
-                    <LikeButton postId={postId} />
-                </div>
                 <div className="w-full grid grid-cols-2 gap-8">
                     {previousPost ? (
                         <Card {...previousPost} text={'이전 게시글'} onRouter={routerHandler} />
@@ -53,7 +44,7 @@ const Mdx_Footer = ({ footMatter }: { footMatter: FootMatterTypes[] }) => {
                     {nextPost ? <Card {...nextPost} text={'다음 게시글'} onRouter={routerHandler} /> : <div></div>}
                 </div>
                 <div className="mt-5">
-                    <CommentComponent />
+                    <Comment />
                 </div>
                 <div className="w-full mt-20 border-b border-black dark:border-gray-400"></div>
                 <CopyRight />
@@ -61,5 +52,4 @@ const Mdx_Footer = ({ footMatter }: { footMatter: FootMatterTypes[] }) => {
         </>
     );
 };
-
 export default Mdx_Footer;
